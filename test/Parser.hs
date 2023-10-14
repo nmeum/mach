ec2d4b827b6d9092ef63fb041d1e0c307b6ef970
Sören Tempel <soeren+git@soeren-tempel.net>
2023-10-14 16:19:15 +0200

    test: Add additional tests for the assignment parser

--- test/Parser.hs
+++ test/Parser.hs
@@ -2,6 +2,7 @@
 
 module Parser (mkParser) where
 
+import Data.Either (isLeft)
 import qualified Data.Sequence as Seq
 import qualified Data.Text as T
 import qualified Mach.Macro as M
@@ -17,6 +18,15 @@ assignTests =
     [ testCase "Simple assignment of macro to string" $
         let rvalue = M.Seq $ Seq.fromList [M.Lit "bar"]
          in parse "foo = bar" @=? assign "foo" M.Delayed rvalue,
+      testCase "Assignment with multiple blanks" $
+        let rvalue = M.Seq $ Seq.fromList [M.Lit "bar"]
+         in parse "foo   =     bar" @=? assign "foo" M.Delayed rvalue,
+      testCase "Assignment with tab character" $
+        assertBool "can only use blanks" $
+          parseErr "foo \t= \tbar",
+      testCase "Assignment without blanks" $
+        assertBool "missing blanks" $
+          parseErr "foo=bar",
       testCase "Simple macro expansion" $
         let rvalue = M.Seq $ Seq.fromList [M.Exp $ M.Lit "BAR"]
          in parse "m_exp = ${BAR}" @=? assign "m_exp" M.Delayed rvalue,
@@ -27,6 +37,9 @@ assignTests =
   where
     parse :: String -> Either Parsec.ParseError M.Assign
     parse = Parsec.parse P.assign ""
+
+    parseErr :: String -> Bool
+    parseErr = isLeft . parse
 
     assign :: T.Text -> M.Flavor -> M.Token -> Either Parsec.ParseError M.Assign
     assign n f t = Right $ M.Assign n f t
