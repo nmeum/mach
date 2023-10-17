4a5736bff687a3d1cf4aac9124ab9666174c1845
Sören Tempel <soeren+git@soeren-tempel.net>
2023-10-17 03:07:58 +0200

    exe: Dump AST for a given Makefile

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,5 +1,20 @@
 module Main where
 
+import Mach.Parser (mkFile)
+import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
+import qualified Text.ParserCombinators.Parsec as P
+
+printAST :: FilePath -> IO ()
+printAST file = do
+  res <- P.parseFromFile mkFile file
+  case res of
+    Left err -> hPutStrLn stderr (show err)
+    Right ast -> putStrLn $ show ast
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  args <- getArgs
+  if length args /= 1
+    then hPutStrLn stderr "Missing file argument"
+    else printAST $ head args
