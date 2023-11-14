5bdfdb78f015a41f8bc9f416dfa348c81339cfe8
Sören Tempel <soeren+git@soeren-tempel.net>
2023-11-14 19:32:14 +0100

    Exec: Fix algorithm for checking if a target is up-to-date

--- lib/Mach/Exec.hs
+++ lib/Mach/Exec.hs
@@ -6,7 +6,6 @@ import Data.Maybe (catMaybes)
 import Mach.Error (MakeErr (..), TargetError (NoTargetOrFile))
 import Mach.Eval
 import System.Directory (doesPathExist, getModificationTime)
-import System.IO.Unsafe (unsafeInterleaveIO)
 
 data FileTarget = FileTarget FilePath TgtDef
 
@@ -14,43 +13,49 @@ lookupTarget :: MkDef -> String -> Maybe FileTarget
 lookupTarget mkDef name =
   FileTarget name <$> lookupRule mkDef name
 
--- Lookup the given target. If neither a target nor a file
--- with the given name exists, then throw an error. If no
--- target but a file exists, return Nothing.
-targetOrFile :: MkDef -> String -> IO (Maybe FileTarget)
-targetOrFile mk name =
-  case lookupTarget mk name of
-    Just x -> pure $ Just x
-    Nothing -> do
-      exists <- doesPathExist name
-      if exists
-        then pure Nothing
-        else throwIO $ TargetErr (NoTargetOrFile name)
+-- Return all prerequisites which represent targets. If a prerequisite
+-- is neither a target nor an existing file, then an error is thrown.
+getTargetPreqs :: MkDef -> FileTarget -> IO [FileTarget]
+getTargetPreqs mk (FileTarget _ target) =
+  catMaybes <$> mapM targetOrFile (getPreqs target)
+  where
+    -- Lookup the given target. If neither a target nor a file
+    -- with the given name exists, then throw an error. If no
+    -- target but a file exists, return Nothing.
+    targetOrFile :: String -> IO (Maybe FileTarget)
+    targetOrFile name =
+      case lookupTarget mk name of
+        Just x -> pure $ Just x
+        Nothing -> do
+          exists <- doesPathExist name
+          if exists
+            then pure Nothing
+            else throwIO $ TargetErr (NoTargetOrFile name)
 
+-- Return the names of all prerequisites that a newer than the given target.
 newerPreqs :: FileTarget -> IO [FilePath]
 newerPreqs (FileTarget name target) = do
   targetTime <- getModificationTime name
-  filterM (fmap (targetTime >) . getModificationTime) $ getPreqs target
+  filterM (fmap (targetTime <) . getModificationTime) $ getPreqs target
 
-------------------------------------------------------------------------
-
--- | Build a given target, including all dependencies that need to be rebuild.
-buildTarget :: MkDef -> FileTarget -> IO ()
-buildTarget mk (FileTarget name target) = do
-  depends <- mapM (targetOrFile mk) $ getPreqs target
-  mapM_ (maybeBuild mk) (catMaybes depends)
-
-  -- Actually build the target itself
-  runCmds $ getCmds mk name target
-
-maybeBuild :: MkDef -> FileTarget -> IO Bool
-maybeBuild mk target@(FileTarget name _) = do
-  targetExists <- doesPathExist name
-
-  -- Make sure newerDepends is lazy evaluated. Otherwise,
-  -- the file may not exist and getModificationTime fails.
-  newerDepends <- unsafeInterleaveIO $ newerPreqs target
-
-  if targetExists && null newerDepends
+-- Check whether the given target is up-to-date, a target shall be
+-- considered up-to-date if it exists and is newer than all of its
+-- dependencies.
+isUp2Date :: FileTarget -> IO Bool
+isUp2Date target@(FileTarget name _) = do
+  exists <- doesPathExist name
+  if not exists
     then pure False
-    else buildTarget mk target >> pure True
+    else null <$> newerPreqs target
+
+-- Build a target if it isn't up-to-date. Returns true if the target
+-- was build or false if it was already up-to-date and wasn't build.
+maybeBuild :: MkDef -> FileTarget -> IO Bool
+maybeBuild mk target@(FileTarget name tgtDef) = do
+  -- Recursively ensure that all prerequisites are up-to-date.
+  getTargetPreqs mk target >>= mapM_ (maybeBuild mk)
+
+  up2Date <- isUp2Date target
+  if up2Date
+    then pure False
+    else runCmds (getCmds mk name tgtDef) >> pure True
