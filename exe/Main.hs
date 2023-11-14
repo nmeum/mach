9c8d1e58c0e22959dbd89292abad8515bbbb81c9
Sören Tempel <soeren+git@soeren-tempel.net>
2023-11-14 20:49:11 +0100

    exe: Start working on option parsing support
    Currently only supports the -f option, nothing else really.

--- exe/Main.hs
+++ exe/Main.hs
@@ -6,8 +6,35 @@ import Mach.Error (MakeErr (..), TargetError (ZeroTargetsDefined))
 import Mach.Eval (MkDef, eval, firstTarget)
 import Mach.Exec (lookupTarget, maybeBuild)
 import Mach.Parser (parseMkFile)
+import System.Console.GetOpt
+  ( ArgDescr (NoArg, ReqArg),
+    ArgOrder (Permute),
+    OptDescr (Option),
+    getOpt,
+    usageInfo,
+  )
 import System.Environment (getArgs)
-import System.IO (hPutStrLn, stderr)
+
+data Flag
+  = EnvOverwrite
+  | Makefile String
+  deriving (Show)
+
+options :: [OptDescr Flag]
+options =
+  [ Option ['f'] [] (ReqArg Makefile "makefile") "Specify a different makefile",
+    Option ['e'] [] (NoArg EnvOverwrite) "Overwrite macro assignments with environment variables"
+  ]
+
+makeOpts :: [String] -> IO ([Flag], [String])
+makeOpts argv =
+  case getOpt Permute options argv of
+    (o, n, []) -> return (o, n)
+    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where
+    header = "Usage: mach [-f makefile] [target_name...]"
+
+------------------------------------------------------------------------
 
 makefile :: FilePath -> IO MkDef
 makefile path = parseMkFile path >>= eval
@@ -22,7 +49,8 @@ runMk path = do
 
 main :: IO ()
 main = do
-  args <- getArgs
-  if length args /= 1
-    then hPutStrLn stderr "Missing file argument"
-    else runMk $ head args
+  (flags, _remain) <- getArgs >>= makeOpts
+  mapM_ runMk $
+    case [f | Makefile f <- flags] of
+      [] -> ["Makefile"]
+      fs -> fs
